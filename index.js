27c845b7e9cf6d94cd547c977a17a6a8fc9d80f6
sagar <wwwghimiresagar88@gmail.com>
2023-08-04 08:01:14 +0545

    before function

---
+++ index.js
@@ -0,0 +1,143 @@
+//for user input  - js interaction
+
+// prompt("enter your name");
+// alert("hii ");
+// confirm("namaste")
+
+
+
+// const name = prompt("enter your name");
+// const password = prompt("enter your password");
+// const cpw = prompt("confirm your password");
+
+// if(password === cpw)
+// {
+//     alert("logged in");
+// }
+// else{
+//    alert(password + "is wrond password");
+    
+// }
+
+
+
+
+// const hour = prompt("enter time");
+// if(hour>=13 && hour<=18)
+// {
+//     alert("class running");
+// }
+// else{
+//     alert("no class");
+// }
+
+//tornory operator
+
+//  const a=2;
+// a>0 ? alert("a is grater than 0") : alert("a is less than 0");
+
+
+// loop 
+
+
+
+// let a=1;
+// for (a;a<10;a++)
+// {
+//     console.log(a);
+// }
+
+
+// let a=2;
+
+// while(a<10)
+// {
+//     console.log(a);
+//     a++;
+// }
+
+
+
+
+
+// do while
+// a=1;
+// do {
+//     console.log(a);
+//     a++;
+// } while (a<=10);
+
+
+
+
+//switch case in js;
+
+// let a= Number (prompt("enter first number"));
+// let b= (prompt("enter condition (/,*,+,-"));
+// let c = Number (prompt("enter second number"));
+// let d;
+
+// switch(b)
+// {
+//     case '+':
+//     {
+//         d=a+c;
+//         console.log(d);
+//     }
+// }
+
+
+
+// function sagar()
+// {
+//    console.log("my name is sagar");
+// }
+// //sagar();
+
+
+// function name(sagar)
+// {
+//     console.log("hello ",sagar);
+// }
+
+// name("sagar");
+// sagar();
+
+
+
+
+// function name()
+// {
+//     console.log("hello");
+// }
+// function name(user)
+// {
+//     console.log("hello ",user);
+// }
+// name();
+// name("user");
+
+
+// es6
+
+
+
+
+
+// const add = (a,b)=>
+// {
+//     console.log(a+b);
+// };
+// add(2,3);
+
+
+
+
+// date
+
+
+// const date = new Date();
+// console.log(date);
+
+
+
